7317427e46d7f5d9792ceaa962d14bad629271fc
MajorDown <romain.fouillaron@gmx.fr>
2023-05-05 23:13:54 +0200

    strucure établie

---
+++ backend/models/sites.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const SitesSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  place: { type: String, required: true },
+  department: { type: Number, required: true },
+  coords: {
+    lon: { type: Number, required: true },
+    lat: { type: Number, required: true },
+  },
+  type: { type: String, required: true },
+  state: { type: String, required: false },
+  publicAccess: { type: Boolean, required: false },
+  description: { type: String, required: false },
+  length: { type: Number, required: false },
+  material: { type: String, required: false },
+  images: {
+    url1: { type: String, required: true },
+    url2: { type: String, required: false },
+    url3: { type: String, required: false },
+    url4: { type: String, required: false },
+    url5: { type: String, required: false },
+  },
+  userId: { type: String, required: true },
+  date: { type: Number, required: true },
+  posts: [{ date: { type: String, required: true } }],
+});
+
+const Sites = mongoose.model("Site", SitesSchema);
+
+module.exports = Sites;
---
+++ backend/routes/sites.routes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { tokenChecker } = require("../middlewares/tokenManager");
+const {
+  imageUploader,
+  imageResizer,
+} = require("../middlewares/sharpAndMulter");
+const {
+  getListOfSites,
+  getSite,
+  createSite,
+  editSite,
+  deleteSite,
+  postOnSite,
+} = require("../controllers/sites.controller");
+const router = express.Router();
+
+router.get("/", getListOfSites);
+router.get("/:id", getSite);
+router.post("/", tokenChecker, imageUploader, imageResizer, createSite);
+router.put("/:id", tokenChecker, imageUploader, imageResizer, editSite);
+router.delete("/:id", tokenChecker, deleteSite);
+router.post("/:id/post", tokenChecker, postOnSite);
+
+module.exports = router;
