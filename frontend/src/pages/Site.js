345f197ffa278578a7e9b8d51d893f73f82f2c95
MajorDown <romain.fouillaron@gmx.fr>
2023-05-25 22:27:08 +0200

    suite accordion

--- frontend/src/pages/Site.js
+++ frontend/src/pages/Site.js
@@ -35,9 +35,22 @@ const Site = () => {
           derrière la mairie. Quelques pancartes relatent son historique autour.
         </p>
         <Accordion name="autres informations">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
-          minima fuga esse ipsum neque aliquam quia accusantium a accusamus
-          similique?
+          <p className="type">Type de structure : Menhir</p>
+          <p className="type">Etat de conservation : bon</p>
+          <p className="type">Accès publique : oui</p>
+          <p className="type">Hauteur : 8m</p>
+          <p className="type">Masse : ~ 80t</p>
+          <p className="type">Matériau : granite</p>
+        </Accordion>
+        <Accordion name="commentaires">
+          <article className="post">
+            <p>Posté par Romano :</p>
+            <p>"super méga menhir de la mort of ze dead"</p>
+          </article>
+          <article className="post">
+            <p>Posté par Alex :</p>
+            <p>"ouai mon chat c'est super méga vrai d'abooooord"</p>
+          </article>
         </Accordion>
       </div>
     </section>
