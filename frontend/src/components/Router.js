4e28036d4b68d9673429c3a18b6b2e4c87b881ee
MajorDown <romain.fouillaron@gmx.fr>
2023-05-29 21:57:52 +0200

    mise en place page create

--- frontend/src/components/Router.js
+++ frontend/src/components/Router.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Accueil from "../pages/Accueil";
 import Site from "../pages/Site";
 import Search from "../pages/Search";
+import Create from "../pages/Create";
 
 const Router = () => {
   return (
@@ -10,6 +11,7 @@ const Router = () => {
       <Route exact path={"/"} Component={Accueil} />
       <Route exact path={"/search"} Component={Search} />
       <Route path="/site" Component={Site} />
+      <Route path="/create" Component={Create} />
       <Route path="*" Component={Accueil} />
     </Routes>
   );
---
+++ frontend/src/pages/Create.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Create = () => {
+  return (
+    <section>
+      <h2>Ajouter un mégalithe</h2>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nobis
+        illum deserunt assumenda, laudantium itaque. Possimus accusamus iure
+        veritatis, quae hic maiores illum quisquam nisi sed deserunt assumenda
+        obcaecati molestiae. Autem natus doloribus explicabo officia at libero,
+        sint, accusantium, facilis dolore non obcaecati deleniti soluta
+        molestiae molestias assumenda sapiente mollitia.
+      </div>
+    </section>
+  );
+};
+
+export default Create;
